359ef72e9486be6ce0706cb85cfc2fa3dbf2bd42
Eliana <guarinoeliana@gmail.com>
2023-04-06 12:21:12 -0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function (api) {
+    api.cache(true)
+    return {
+        presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
+        plugins: [
+            "react-native-reanimated/plugin",
+            [
+                "module:react-native-dotenv",
+                {
+                    envName: "APP_ENV",
+                    moduleName: "@env",
+                    path: ".env",
+                    safe: false,
+                    allowUndefined: true,
+                    verbose: false,
+                },
+            ],
+        ],
+    }
+}
---
+++ navigation/DrawerNavigation.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { createDrawerNavigator } from "@react-navigation/drawer"
+import Home from "../screens/Home"
+
+const Drawer = createDrawerNavigator()
+
+export default function DrawerNavigation() {
+    return (
+        <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen name="Home" component={Home} />
+        </Drawer.Navigator>
+    )
+}
