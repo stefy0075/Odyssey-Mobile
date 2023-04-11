5993cea833e2e5832831c7c8ca06a514034da0cb
Alexis Franco <ronaldofranco196@gmail.com>
2023-04-11 12:06:58 -0300

    fix

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { View, ImageBackground } from "react-native";
 import styles from "../styles/SignUp.style.js";
 import SignUpForm from "../components/SignUpForm.js";
+import Logout from "../components/Logout.js";
 
 export default class SignUp extends Component {
   render() {
@@ -12,6 +13,7 @@ export default class SignUp extends Component {
         resizeMode="cover"
       >
         <View style={styles.container}>
+          {/* <Logout/> */}
           <SignUpForm/>
         </View>
       </ImageBackground>
